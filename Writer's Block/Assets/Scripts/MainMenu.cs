f74e7db6a1cba6f26d039e36f93e953213518bef
Matt Loyola <mjloyola@ucsc.edu>
2017-11-12 19:32:14 -0800

    Simple Main Menue and Phone Menu with Sounds

---
+++ Writer's Block/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	//prototype level designator
+    public string startLevel;
+
+	void Start()
+	{
+
+	}
+
+    //start new game on prototype level
+    public void NewGame()
+    {
+        SceneManager.LoadScene(startLevel);
+    }
+
+    //close game when in application mode
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
